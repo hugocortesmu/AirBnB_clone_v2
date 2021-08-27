cfa47912f2f9d517e225f0071af107121d2d5a5b
Hugo Cortes <hugocortesmu@gmail.com>
2021-08-26 23:30:07 -0500

    comment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Write a Fabric script (based on the file 1-pack_web_static.py)"""
+""""""
 import os
 from fabric.api import put, run, env
 from datetime import datetime
 
 
 def do_deploy(archive_path):
-    '''distributes an archive to your web servers, using the function do_deploy
+    '''
+    distributes an archive to your web servers, using the function do_deploy
     '''
